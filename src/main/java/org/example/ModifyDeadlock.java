6e0bc876e22c2eb0189d0770e59f0e0a5ed2f652
munkei <mk.chong5@gmail.com>
2025-04-27 21:22:13 +0800

    Initial commit

---
+++ src/main/java/org/example/ModifyDeadlock.java
@@ -0,0 +1,35 @@
+package org.example;
+
+import java.util.Random;
+
+public class ModifyDeadlock implements Runnable {
+    private static final Object resource1 = new Object();
+    private static final Object resource2 = new Object();
+    private final Random random = new Random(System.currentTimeMillis());
+
+    public static void main(String[] args) {
+        Thread myThread1 = new Thread(new ModifyDeadlock(), "thread-1");
+        Thread myThread2 = new Thread(new ModifyDeadlock(), "thread-2");
+        myThread1.start();
+        myThread2.start();
+    }
+
+    public void run() {
+        for (int i = 0; i < 10000; i++) {
+            System.out.println("[" + Thread.currentThread().getName() + "] Trying to lock resource 1.");
+            synchronized (resource1) {
+                System.out.println("[" + Thread.currentThread().getName() + "] Locked resource 1.");
+                try {
+                    Thread.sleep(1);
+                } catch (InterruptedException e) {
+                    Thread.currentThread().interrupt();
+                }
+                System.out.println("[" + Thread.currentThread().getName() + "] Trying to lock resource 2.");
+                synchronized (resource2) {
+                    System.out.println("[" + Thread.currentThread().getName() + "] Locked resource 2.");
+
+                }
+            }
+        }
+    }
+}
